99fa6e03d8a41eda088900ff923585cbe4d95b5e
Huseyin Ocal <huseyin.ocal@tubitak.gov.tr>
2021-11-06 14:59:02 +0300

    007-add encrypt key and @RefreshScope annotation to all service.

--- loan/src/main/java/huseyin/ocal/loan/LoanApplication.java
+++ loan/src/main/java/huseyin/ocal/loan/LoanApplication.java
@@ -3,9 +3,11 @@ package huseyin.ocal.loan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@RefreshScope
 @EnableJpaRepositories(basePackages = "huseyin.ocal.loan.repository")
 @EntityScan(basePackages = "huseyin.ocal.loan.entity")
 public class LoanApplication {
