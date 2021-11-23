a15e580e34d29ab23a3179c0a9a5029659adf9a9
Huseyin Ocal <huseyin.ocal@tubitak.gov.tr>
2021-11-23 08:22:28 +0300

    018-setup micrometer.

--- account/src/main/java/huseyin/ocal/account/AccountApplication.java
+++ account/src/main/java/huseyin/ocal/account/AccountApplication.java
@@ -1,10 +1,13 @@
 package huseyin.ocal.account;
 
+import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
@@ -18,4 +21,8 @@ public class AccountApplication {
         SpringApplication.run(AccountApplication.class, args);
     }
 
+    @Bean
+    public TimedAspect timedAspect(MeterRegistry registry) {
+        return new TimedAspect(registry);
+    }
 }
